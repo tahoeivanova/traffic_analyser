20defcd9bf1ccc497ced1613af14d486a573bc4e
kali-wsy <tahoeivanova@gmail.com>
2022-05-08 03:02:07 -0500

    feat (tcp/udp) Added tcp/udp analyzing functions

--- sniffpackets.py
+++ sniffpackets.py
@@ -9,6 +9,63 @@ import binascii
 sock_created = False
 sniffer_socket = 0
 
+
+def analyze_udp_header(data_recv):
+	udp_hdr = struct.unpack('!4H', data_recv[:8])
+	src_port = udp_hdr[0]
+	dst_port = udp_hdr[1]
+	length = udp_hdr[2]
+	checksum = udp_hdr[3]
+	data = data_recv[8:]
+
+
+	print("_____________________UDP HEADER________________")
+	print("Source %hu" % src_port)
+	print("Destination %hu" % dst_port)
+	print("Length: %hu" % length)
+	print("Checksum: %hu" % checksum)
+
+	return data
+
+
+def analyze_tcp_header(data_recv):
+	tcp_hdr = struct.unpack('!2H2I4H', data_recv[:20])
+	src_port = tcp_hdr[0]
+	dst_port = tcp_hdr[1]
+	seq_num = tcp_hdr[2]
+	ack_num = tcp_hdr[3]
+	data_offset = tcp_hdr[4] >> 12
+	reserved = (tcp_hdr[5] >> 6) & 0x03ff
+	flags = tcp_hdr[4] & 0x003f
+	window = tcp_hdr[5]
+	checksum = tcp_hdr[6]
+	urg_ptr = tcp_hdr[7]
+	data = data_recv[20:]
+
+	urg = bool(flags & 0x0020)
+	ack = bool(flags & 0x0010)
+	psh = bool(flags & 0x0008)
+	rst = bool(flags & 0x0004)
+	syn = bool(flags & 0x0002)
+	fin = bool(flags & 0x0001)
+
+
+	print("_____________________TCP HEADER________________")
+	print("Source %hu" % src_port)
+	print("Destination %hu" % dst_port)
+	print("Seq: %u" % seq_num)
+	print("Ack: % u" % ack_num)
+	print("URG: %d" % urg)
+	print("ACK: %d" % ack)
+	print("PSH: %d" % psh)
+	print("RST: %d" % rst)
+	print("SYN: %d" % syn)
+	print("FIN: %d" % fin)
+	print("Winsize: %hu" % window)
+	print("Checksum %hu" % checksum)
+	return data
+
+
 def analyze_ip_header(data_recv):
 	ip_hdr = struct.unpack('!6H4s4s', data_recv[:20]) # 20 bytes - ip protocol header size
 	ver = ip_hdr[0] >> 12
@@ -53,8 +110,8 @@ def analyze_ether_header(data_recv):
 	ip_bool = False
 
 	eth_hdr = struct.unpack('!6s6sH', data_recv[:14]) # take 1st 14 bytes (ethernet protocol header size)
-	dest_mac = binascii.hexlify(eth_hdr[0]) # 1st 6 bites
-	src_mac = binascii.hexlify(eth_hdr[1])
+	dest_mac = binascii.hexlify(eth_hdr[0]).decode("UTF-8") # 1st 6 bites
+	src_mac = binascii.hexlify(eth_hdr[1]).decode("UTF-8")
 	proto = eth_hdr[2] >> 8
 	data = data_recv[14:]
 
@@ -87,11 +144,11 @@ def main():
 	else:
 		return
 
-	#if tcp_udp == "TCP":
-	#	data_recv = analyze_tcp_header(data_recv)
-	#elif tcp_udp == "UDP":
-	#	data_recv = analyze_upd_header(data_recv)
-	#else:
+	if tcp_udp == "TCP":
+		data_recv = analyze_tcp_header(data_recv)
+	elif tcp_udp == "UDP":
+		data_recv = analyze_udp_header(data_recv)
+	else:
 		return
 
 while True:
